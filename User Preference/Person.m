85ea4a97fc7bcf2af1f256e0b50692b52814cad5
Jason Ash <jash@esi-estech.com>
2014-10-15 12:19:17 -0500

    Added child pane specifier

--- User Preference/Person.m
+++ User Preference/Person.m
@@ -35,22 +35,20 @@ static NSString *kSegmentColorKey = @"IADSegmentColor";
 
 - (void)saveDataToUserDefaults
 {
-    [self.userDefaults setObject:self.nameString forKey:kUserNameKey];
-    [self.userDefaults setFloat:self.numberFloat forKey:kPassCodeKey];
-    [self.userDefaults setInteger:self.stepperInteger forKey:kUserStepperKey];
-    [self.userDefaults setBool:self.switchBool forKey:kUserSwitchKey];
+   [self.userDefaults setObject:self.nameString forKey:kUserNameKey];
+   [self.userDefaults setFloat:self.numberFloat forKey:kPassCodeKey];
+   [self.userDefaults setInteger:self.stepperInteger forKey:kUserStepperKey];
+   [self.userDefaults setBool:self.switchBool forKey:kUserSwitchKey];
 	[self.userDefaults setObject:self.segmentString forKey:kSegmentColorKey];
 }
 
 - (void)loadDataFromUserDefaults
 {
-    self.nameString = [self.userDefaults stringForKey:kUserNameKey];
-    self.numberFloat = [self.userDefaults floatForKey:kPassCodeKey];
-    self.stepperInteger = [self.userDefaults integerForKey:kUserStepperKey];
-    self.switchBool = [self.userDefaults boolForKey:kUserSwitchKey];
+   self.nameString = [self.userDefaults stringForKey:kUserNameKey];
+   self.numberFloat = [self.userDefaults floatForKey:kPassCodeKey];
+   self.stepperInteger = [self.userDefaults integerForKey:kUserStepperKey];
+   self.switchBool = [self.userDefaults boolForKey:kUserSwitchKey];
 	self.segmentString = [self.userDefaults stringForKey:kSegmentColorKey];
-	NSLog(@"name: %@", self.nameString);
-    NSLog(@"key: %@", self.segmentString);
 }
 
 @end
--- User Preference/ViewController.swift
+++ User Preference/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
 		notificationCenter.addObserver(self, selector: "applicationWillTerminate:", name: UIApplicationWillTerminateNotification, object: app)
 		notificationCenter.addObserver(self, selector: "colorSetAlert:", name: "colorSetNotification", object: nil)
         
-        segmentedControl.addTarget(self, action: "setColorKey:", forControlEvents: .ValueChanged)
+      segmentedControl.addTarget(self, action: "setColorKey:", forControlEvents: .ValueChanged)
 		
 		person.loadDataFromUserDefaults()
 	}
@@ -56,41 +56,44 @@ class ViewController: UIViewController {
 	}
     
 	func setColorKey(value : Int) {
-		println("fromSegment: ", value)
 		progressView.setProgress(100, animated: false)
 		switch (segmentedControl.selectedSegmentIndex) {
 		case 0:
 			person.segmentString = "red"
-			segmentedControl.tintColor = UIColor.redColor()
-			progressView.progressTintColor = UIColor.redColor()
+         changeObjectsColor(UIColor.redColor())
 			break
 		case 1:
 			person.segmentString = "orange"
-			segmentedControl.tintColor = UIColor.orangeColor()
-			progressView.progressTintColor = UIColor.orangeColor()
+         changeObjectsColor(UIColor.orangeColor())
 			break
 		case 2:
 			person.segmentString = "yellow"
-			segmentedControl.tintColor = UIColor.yellowColor()
-			progressView.progressTintColor = UIColor.yellowColor()
+         changeObjectsColor(UIColor.yellowColor())
 			break
 		default:
 			person.segmentString = "red"
-			segmentedControl.tintColor = UIColor.redColor()
-			progressView.progressTintColor = UIColor.redColor()
+         changeObjectsColor(UIColor.redColor())
 			break
 		}
 		if (value != 0) {
 			notificationCenter.postNotificationName("colorSetNotification", object : nil)
 		}
     }
+   
+   func changeObjectsColor(color : UIColor) {
+      segmentedControl.tintColor = color
+      progressView.progressTintColor = color
+      stepper.tintColor = color
+      viewSwitch.tintColor = color
+      viewSwitch.onTintColor = color
+   }
 
 	func displayModelData() {
 		nameText.text = person.nameString
 		numberTextField.text = "\(person.numberFloat)"
 		stepper.value = Double(person.stepperInteger)
 		viewSwitch.on = person.switchBool
-		println("segmentKeyString: ", "\(person.segmentString)")
+      if (person.segmentString != nil) {
         switch (person.segmentString) {
             case "red":
                 segmentedControl.selectedSegmentIndex = 0
@@ -104,10 +107,10 @@ class ViewController: UIViewController {
         default:
             segmentedControl.selectedSegmentIndex = 0
         }
+      }
 	}
 	
 	func updateModelData() {
-		println("updateModelData")
 		person.nameString = nameText.text
 		person.numberFloat = (numberTextField.text as NSString).floatValue
 		person.stepperInteger = Int(stepper.stepValue)
